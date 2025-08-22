543048b97b997d00ce849a4471130e7ff6747553
Tin de Zeeuw <tin.de.zeeuw@alliander.com>
2025-08-22 11:16:03 +0200

    trying to get a simple proxy with http call working... :'(

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,30 @@
-<script setup lang="ts"></script>
-
 <template>
   <h1>You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+
+  <div>
+    <button @click="fetchData">Fetch Data</button>
+  </div>
 </template>
 
 <style scoped></style>
+
+<script lang="ts">
+export default {
+  methods: {
+    async fetchData() {
+      fetch('/api/alive', {
+        referrerPolicy: 'same-origin', // no-referrer-when-downgrade, no-referrer, origin, same-origin...
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          debugger
+          console.log(data)
+        })
+    },
+  },
+}
+</script>
